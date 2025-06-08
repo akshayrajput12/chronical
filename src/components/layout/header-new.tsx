9bf7e0514f063a0af3a01d72378b521c9f0a1ea6
chronicleexhibits12 <chronicleexhibits12@gmail.com>
2025-06-08 23:12:37 +0530

    header

--- src/components/layout/header-new.tsx
+++ src/components/layout/header-new.tsx
@@ -229,7 +229,7 @@ const Header = () => {
                                 </div>
 
                                 {/* Right Navigation */}
-                                <div className="hidden lg:flex items-center justify-end flex-1">
+                                <div className="hidden lg:flex items-center justify-end flex-1 -mr-[125px] xl:-mr-[125px] 2xl:-mr-[125px]">
                                     <div className="flex items-center space-x-3 xl:space-x-5">
                                         <NavItem
                                             href="/about"
@@ -294,22 +294,22 @@ const Header = () => {
                                         )}
                                     >
                                         <Logo isScrolled={mounted} />
-                                        <div className="flex items-center space-x-3">
+                                        <div className="flex items-center space-x-3 flex-shrink-0">
                                             {/* Mobile WhatsApp Button */}
                                             <Link
                                                 href="https://wa.me/yournumberhere"
-                                                className="bg-transparent border border-[#a5cd39] text-[#a5cd39] hover:bg-[#a5cd39] hover:text-white transition-colors duration-300 rounded px-2 py-1 flex items-center space-x-1 text-xs"
+                                                className="bg-transparent border border-[#a5cd39] text-[#a5cd39] hover:bg-[#a5cd39] hover:text-white transition-colors duration-300 rounded px-2 py-1 sm:px-3 sm:py-2 md:px-3 md:py-2 flex items-center space-x-1 sm:space-x-2 text-xs sm:text-sm md:text-sm whitespace-nowrap flex-shrink-0"
                                             >
                                                 <svg
                                                     xmlns="http://www.w3.org/2000/svg"
-                                                    className="h-3 w-3"
+                                                    className="h-3 w-3 sm:h-4 sm:w-4 md:h-4 md:w-4"
                                                     fill="currentColor"
                                                     viewBox="0 0 24 24"
                                                 >
                                                     <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
                                                 </svg>
-                                                <span className="hidden sm:inline">
-                                                    WA
+                                                <span className="hidden sm:inline md:inline">
+                                                    WHATSAPP
                                                 </span>
                                             </Link>
                                             <button
